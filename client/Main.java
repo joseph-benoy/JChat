4004199c01117074384966cc5d9f1e42995c0a58
joseph-benoy <josephbenoy03@gmail.com>
2022-01-01 14:29:16 +0530

    fixed server class

---
+++ client/Main.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.time.LocalTime;
+public class Main {
+    public static void main(String args[]){
+        
+    }
+}
